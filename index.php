c4c3d690ce852b4dd4ffa8a33278c34a9d507b7f
aytohuetorvega <cruzjerezantonio@gmail.com>
2017-12-13 13:39:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
 <?php
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-$conn = new mysqli($server, $username, $password, $db);
-echo "Mostrando los datos de conexión: <br>";
-echo $server; echo "<br>";
-echo $username; echo "<br>";
-echo $password; echo "<br>";
-echo $db; echo "<br>";
-
-
-
-
 
 /*
  *---------------------------------------------------------------
